f8eb0b9e3c564145e2fd8f4b2ee50483698869e6
Vanlocnong <95829802+Vanlocnong@users.noreply.github.com>
2022-01-08 13:27:15 +0700

    1
    1

---
+++ lesson1__khaibaobien/main.js
@@ -0,0 +1,9 @@
+// KHAI BÁO BIẾN
+
+var fullName = "Nong Van Loc";
+
+var AGE = 26;
+
+alert(fullName)
+
+alert(AGE)
---
+++ lesson3__toantu/main.js
@@ -0,0 +1,12 @@
+/*
+
+MỘT SỐ HÀM BUILT-IN
+    1.alert-tạo ra 1 msg box
+    2.console.log-in ra giá trị
+    3.confirm-tạo ra 1 msg box dạng xác nhận
+    4.prompt- ==hàm confirm nhưng có thể lấy thêm giá trị người dùng
+    5.setTimeout-sau 1 khoảng thời gian sẽ thực hiện 1 đoạn code nào đó
+    6.setInterval- thực hiện đoạn code liên tục sau 1 khoảng thời gian setup sẵn
+*/
+
+console.log("1")
