6443b3edafcfa1efdb5d9c092ba716e98e08cc84
Vanlocnong <95829802+Vanlocnong@users.noreply.github.com>
2022-01-08 13:43:06 +0700

    1
    1

--- lesson3__toantu/main.js
+++ lesson3__toantu/main.js
@@ -1,12 +1,28 @@
 /*
 
-MỘT SỐ HÀM BUILT-IN
-    1.alert-tạo ra 1 msg box
-    2.console.log-in ra giá trị
-    3.confirm-tạo ra 1 msg box dạng xác nhận
-    4.prompt- ==hàm confirm nhưng có thể lấy thêm giá trị người dùng
-    5.setTimeout-sau 1 khoảng thời gian sẽ thực hiện 1 đoạn code nào đó
-    6.setInterval- thực hiện đoạn code liên tục sau 1 khoảng thời gian setup sẵn
+1. toán tử số học
+2.toán tử gán
+3.toán tử so sanhs
+4.toán tử logic
+
 */
 
-console.log("1")
+/* TOAN TU SỐ HỌC
+
++ CỘNG
+
+- TRỪ
+
+* NHÂN 
+
+* LŨY THỪA
+
+/ CHIA
+
+% CHIA LẤY DƯ
+
+++ TĂNG GIÁ TRỊ 1 SỐ
+
+-- GIẢM GIÁ TRỊ MỘT SỐ
+*/
+
